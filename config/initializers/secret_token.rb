8ffd0cb1ad56c658a8f249a94b34e09f8a8d8799
fernanda <fernanda@fernanda-RV415-RV515.(none)>
2012-11-14 01:21:01 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prova2::Application.config.secret_token = '98dbbfd1d511816d4e67a2f01976137b76688430973bbb4bf1c371d94d175d6d5c40fd48e3a1213e267316e04c317cad327dc69279ed70215383243829c5cff7'
